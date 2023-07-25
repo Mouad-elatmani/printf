86ac474382a3705a868a33976b7be5879b2fbc2c
Mouad-elatmani <96914263+Mouad-elatmani@users.noreply.github.com>
2023-07-25 23:47:29 +0100

    Update _printf.c
    Signed-off-by: Mouad-elatmani <96914263+Mouad-elatmani@users.noreply.github.com>

--- _printf.c
+++ _printf.c
@@ -10,37 +10,35 @@
  * @format: string with format specifier
  * Return: number of characters printed
  */
-
-
 int _printf(const char *format, ...)
 {
-	int i, count = 0;
-	va_list list;
-	int (*m)(va_list);
+        int i, count = 0;
+        va_list list;
+        int (*m)(va_list);
 
-	if (format != NULL)
-	{
-		va_start(list, format);
+        if (format != NULL)
+        {
+                va_start(list, format);
 
-		for (i = 0; format != NULL && format[i] != '\0'; i++)
-		{
-			if (format[i] == '%')
-			{
-				m = func_select(format[i + 1]);
-				if (m)
-					count += m(list);
-				else
-				{
-					 count += _putchar(format[i]);
-					 count += _putchar(format[i + 1]);
-				}
-				 i++;
-			}
-			else
-				 count += _putchar(format[i]);
-		}
-		va_end(list);
-		return (count);
-	}
-	return (-1);
+                for (i = 0; format != NULL && format[i] != '\0'; i++)
+                {
+                        if (format[i] == '%')
+                        {
+                                m = func_select(format[i + 1]);
+                                if (m)
+                                        count += m(list);
+                                else
+                                {
+                                         count += _putchar(format[i]);
+                                         count += _putchar(format[i + 1]);
+                                }
+                                 i++;
+                        }
+                        else
+                                 count += _putchar(format[i]);
+                }
+                va_end(list);
+                return (count);
+        }
+        return (-1);
 }
