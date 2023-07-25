a6f83ec74aacd8261750c43b5d367d2933ba964c
dohaMST <mastourdoha.dev001@gmail.com>
2023-07-25 16:07:09 -0700

    tt

--- _printf.c
+++ _printf.c
@@ -14,41 +14,33 @@
 
 int _printf(const char *format, ...)
 {
-        int i, count = 0;
-        va_list list;
-        int (*m)(va_list);
+	int i, count = 0;
+	va_list list;
+	int (*m)(va_list);
 
-        if (format != NULL)
-        {
-                va_start(list, format);
+	if (format != NULL)
+	{
+		va_start(list, format);
 
-                for (i = 0; format != NULL && format[i] != '\0'; i++)
-                {
-                        if (format[i] == '%')
-                        {
-                                if (format[i + 1] == '%')
-                                {
-                                        count += _putchar(format[i]);
-                                        i++;
-                                }
-                                else
-                                {
-                                        m = func_select(format[i + 1]);
-                                        if (m)
-                                                count += m(list);
-                                        else
-                                        {
-                                                 count += _putchar(format[i]);
-                                                 count += _putchar(format[i + 1]);
-                                        }
-                                         i++;
-                                }
-                        }
-                        else
-                                 count += _putchar(format[i]);
-                }
-                va_end(list);
-                return (count);
-        }
-        return (-1);
+		for (i = 0; format != NULL && format[i] != '\0'; i++)
+		{
+			if (format[i] == '%')
+			{
+				m = func_select(format[i + 1]);
+				if (m)
+					count += m(list);
+				else
+				{
+					 count += _putchar(format[i]);
+					 count += _putchar(format[i + 1]);
+				}
+				 i++;
+			}
+			else
+				 count += _putchar(format[i]);
+		}
+		va_end(list);
+		return (count);
+	}
+	return (-1);
 }
