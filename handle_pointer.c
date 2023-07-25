95b80b373fc6e8714005178ec2bd987db4902d7b
dohaMST <doha.mastour@gmail.com>
2023-07-24 22:34:04 -0700

    task of pointer

--- handle_pointer.c
+++ handle_pointer.c
@@ -1,42 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdio.h>
-#include <limits.h>
-
-
-/**
- * helper_xP - a function that helps
- * @bcount: count
- * @num: num
- * Return:nothing
-*/
-char *helper_xP(int bcount, unsigned long num)
-{
-	int reminder, i;
-	char binary, *s;
-
-	s  = malloc(sizeof(char) * bcount + 1);
-	if (s == NULL)
-		return (s);
-	for (i = bcount - 1; i != -1; i--)
-	{
-		reminder = num % 16;
-
-		if (reminder > 9)
-			binary = (reminder - 10) + 'a';
-		else
-			binary = reminder + '0';
-		s[i] = binary;
-		num = num / 16;
-	}
-	s[bcount] = '\0';
-
-	return (s);
-}
 
 /**
  * handle_pointer - a function that writes signed hexadecimal integer
@@ -46,32 +8,28 @@ char *helper_xP(int bcount, unsigned long num)
 int handle_pointer(va_list args)
 {
 
-	unsigned long  num, temp;
-	int bcount;
+	unsigned long num = (unsigned long) va_arg(args, void *);
+	int bcount = 0, i;
 	char *s;
+	unsigned long temp;
 
-	bcount = 0;
-	num = (unsigned long) va_arg(args, void *);
 	if (num == 0)
-	{
-		bcount += write(STDOUT_FILENO, "(nil)", 5);
-		return (bcount);
-	}
-	bcount += write(STDOUT_FILENO, "0x", 2);
-	temp = num;
-	bcount = 0;
-	while (temp != 0)
-	{
-		temp = temp / 16;
+		return (write(STDOUT_FILENO, "(nil)", 5) + 2);
+
+	for (temp = num; temp != 0; temp /= 16)
 		bcount++;
-	}
-	s = helper_xP((bcount), num);
+
+	s  = malloc(sizeof(char) * bcount + 1);
 	if (s == NULL)
 		return (0);
 
+	for (i = bcount - 1; i >= 0; i--, num /= 16)
+		s[i] = num % 16 + ((num % 16 > 9) ? 'a' - 10 : '0');
 
-	bcount =  write(STDOUT_FILENO, s, bcount);
+	s[bcount] = '\0';
+	bcount =  write(STDOUT_FILENO, "0x", 2) + write(STDOUT_FILENO, s, bcount);
 	free(s);
 
-	return (bcount + 2);
+	return (bcount);
 }
+
